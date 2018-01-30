d9d9e500eae07828ffea9eb5f97264d4efadb995
Sydney McAuliffe <SydneyMcAuliffe@Sydneys-MacBook-Pro.local>
2018-01-30 14:54:19 -0500

    changed some stuff

--- sketch.js
+++ sketch.js
@@ -1,31 +1,38 @@
-function setup(){
-    createCanvas(800,800);
-}
-
-function draw(){
-    fill(255,200,200);
-    rect(400,400, 200, 200);
-}
-
 var teapot;
+var x = 100.0; 
+var y = 100; 
+var speed = 1.5; 
 
-function setup() {
-    createCanvas(100,100,WEBGL);
-
-    teapot = loadModel('assets/teapot.obj');
+function setup(){
+    createCanvas(1000, 1000);
+    
 }
 
-function draw () {
-    background (200);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01)
-    model(teapot);
-}
-
-function setup () {
-    createCanvas (800,800);
-}
 function draw(){
-    fill (255,200,200)
-    triangle (30, 75, 86, 20, 86, 75);
+    background(0);
+
+    //this is rectangle
+    fill(255,200,200);
+    rect(x,y, 200, 200);
+
+    //this is triangle
+    fill(255,200,200)
+    triangle (x, y, 86, 20, 86, 75);
+
+    //this is ellipse
+    fill(1);
+    x = x + speed; 
+    if(x > width){
+      x = 0; 
+    }
+    fill(210, 30, 140);
+    ellipse(x,y,50,50);
+    
+     //this is teapot
+//     rotateX(frameCount * 0.01);
+//     rotateY(frameCount * 0.01)
+//     model(teapot);
+// 
+
 }
+
