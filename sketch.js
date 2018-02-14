733f44c82d54466b5c2429b0c9e64a86b261dc62
Sydney McAuliffe <SydneyMcAuliffe@Sydneys-MacBook-Pro.local>
2018-02-14 00:08:50 -0500

    arrays

--- sketch.js
+++ sketch.js
@@ -1,30 +1,61 @@
-var img; 
-var y= 100;
+var bugs = []; // array of Jitter objects
 
-function setup(){
-	img = loadImage('assets/IMG_9682.jpg')
-	createCanvas(windowWidth, windowHeight)
-	background(0,0,255)
-	stroke(255);
-	frameRate(30);
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  // Create objects
+  for (var i=0; i<300; i++) {
+      if (i%3==0) {
+        bugs.push(new Jitter("#1a1a1a")); 
+      }
+      else if (i%2==0) {
+        bugs.push(new Jitter("#ffffff")); 
+      }
+      else if (i%5==0) {
+        bugs.push(new Jitter("#ff0000")); 
+      }
+      else {  bugs.push(new Jitter("#008000")); }
+  }
+  bg = loadImage("libraries/art.jpg");
 }
 
-function draw(){
-image(img,0,0,windowWidth, windowHeight)
-rect(40,30,60,60)
-ellipse(700,100,70,70)
-textAlign(CENTER);
-textSize(40);
-text("Camino", windowWidth/2, 300);
-textSize(12);
-text("Those who come to the Camino with a heavy heart leave with a lighter load. Don't cry because it's over, smile because it happened. As in any journey, it's not what you take with you, but what you leave behind. Listen to your heart and it will show you the way.", windowWidth/2, 400);
-y=y-1;
-if (y<0) {
-	y=height;
-}
-line(0,y,width,y);
-  fill(random(0, 255), random(0, 255), random(0, 255));
-  ellipse(mouseX,mouseY,50,50);
+function draw() {
+  background(bg);
+  for (var i=0; i<bugs.length; i++) {
+    bugs[i].move();
+    bugs[i].display();
+  }
 }
 
+// Jitter class
+function Jitter(color) {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(10, 30);
+  this.speed = 1;
+  this.color=color;
 
+  this.move = function() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  this.display = function() {
+    star(this.x, this.y, this.diameter, this.diameter,6,this.color);
+  };
+}
+
+function star(x, y, radius1, radius2, npoints,color) {
+    var angle = TWO_PI / npoints;
+    var halfAngle = angle/2.0;
+    beginShape();
+    for (var a = 0; a < TWO_PI; a += angle) {
+      var sx = x + cos(a) * radius2;
+      var sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a+halfAngle) * radius1;
+      sy = y + sin(a+halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    fill(color);
+    endShape(CLOSE);
+  }
